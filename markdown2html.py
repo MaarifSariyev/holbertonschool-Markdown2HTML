3e190291a06c90b9bd4e1993675f4a7a40566e67
MaarifSariyev <151127745+MaarifSariyev@users.noreply.github.com>
2024-10-09 19:18:27 +0400

    Update markdown2html.py

--- markdown2html.py
+++ markdown2html.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python3
-
 import sys
 import os
 
@@ -9,21 +7,17 @@ def main():
     
     Usage: ./markdown2html.py input.md output.html
     """
-    # Check for the correct number of arguments
     if len(sys.argv) < 3:
         print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
         sys.exit(1)
     
-    # Get the input and output file names
     input_file = sys.argv[1]
     output_file = sys.argv[2]
 
-    # Check if the Markdown file exists
     if not os.path.exists(input_file):
         print(f"Missing {input_file}", file=sys.stderr)
         sys.exit(1)
     
-    # If everything is fine, exit with code 0
     sys.exit(0)
 
 if __name__ == "__main__":
