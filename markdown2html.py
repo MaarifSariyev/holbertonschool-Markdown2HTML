e7cde45488d276bd1e8ed5c6bc1ef952ee861d25
MaarifSariyev <151127745+MaarifSariyev@users.noreply.github.com>
2024-10-09 19:17:35 +0400

    Update markdown2html.py

--- markdown2html.py
+++ markdown2html.py
@@ -1,9 +1,14 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 
 import sys
 import os
 
 def main():
+    """
+    Converts a Markdown file to HTML format.
+    
+    Usage: ./markdown2html.py input.md output.html
+    """
     # Check for the correct number of arguments
     if len(sys.argv) < 3:
         print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
